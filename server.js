982ed639a73dcf981112bedcec4e44106afa8792
Arad David <arado3333@gmail.com>
2025-06-11 00:27:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const server = express();
 const PORT = process.env.PORT || 3000;
 const limiter = rateLimit({
     windowMs: 60 * 60 * 1000, // 60 minutes
-    limit: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    limit: 50, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
     standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
 });
