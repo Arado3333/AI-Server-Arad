cd4d7121914dc76814856d174680fdca6d7532f5
Arad David <arado3333@gmail.com>
2025-06-11 09:47:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -56,7 +56,6 @@ server.post("/api/chat", async (req, res) => {
 });
 
 server.post("/api/generate", async (req, res) => {
-    // console.log(req.body.contents);
     try {
         const response = await ai.models.generateContent({
             model: "gemini-2.0-flash",
@@ -64,17 +63,14 @@ server.post("/api/generate", async (req, res) => {
             config: {
                 maxOutputTokens: 1000,
                 temperature: 0.1,
-                systemInstruction:
-                    "You are a tailwindcss wizard! Master of tailwindcss, developers need your help with designing their websites with tailwindcss quickly and effortlessly. Your job is to process the developer's request, i.e what element he wants to make or what design he wants to implement, and in response, give him the exact tailwindcss code to acheive his desired design. if it's complex components that needs recreation, provide fresh html code with the necessary elements and styles applied in tailwindcss class names. the response should be concise, on-point and without hallucinations. if you can't provide some style code for certain request, provide code that reaches the desired design as close as possible. for class names only, i.e if not a whole component, wrap the response text inside a <span></span> element. return text like this: <span>YOUR_TAILWIND_CODE_HERE</span>. For new components, generate the html code as inside a <section></section> element. make it suitable for react jsx",
+                systemInstruction: process.env.SYSTEM_PROMPT,
             },
         });
         let text = response.text;
-        // console.log(text);
 
         res.status(200).send(text);
     } catch (error) {
         res.status(500).json({ error: "Error sending message to gemini" });
-        console.log(error);
     }
 });
 
